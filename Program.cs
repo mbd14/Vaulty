4c475e5cd84cfdb448ad7c3bb7c1f7299e11f7b3
mbd14 <mohamed.dris.bendriss@gmail.com>
2025-04-05 21:13:14 +0200

    Base Bot with simple info slash command

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Vaulty
+{
+    internal class Program
+    {
+        // Instance of the Vaulty App
+        public App.Vaulty vaulty;
+
+        // Main method for the program
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        // Main method for async execution of the bot
+        static async Task MainAsync()
+        {
+            // Start the App and wait forever in the main thread
+            App.Vaulty vaulty = new App.Vaulty();
+            vaulty.Start();
+            await Task.Delay(-1);
+        }
+    }
+}
---
+++ Utils/Const.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vaulty.Utils
+{
+    public class Const
+    {
+        public static readonly string TOKEN_FILE = "sensitive.json";
+        public static readonly string INFO_FILE = "info.json";
+    }
+}
