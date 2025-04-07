2350800fee7cb79551c122e935cefd8eae44fc06
mbd14 <mohamed.dris.bendriss@gmail.com>
2025-04-07 21:23:33 +0200

    Modified info command embed managment to use the VaultyEmbed wrapper class

--- Modules/InfoModule.cs
+++ Modules/InfoModule.cs
@@ -2,6 +2,7 @@
 using DSharpPlus.Commands;
 using DSharpPlus.Entities;
 using DSharpPlus.SlashCommands;
+using Vaulty.Embeds;
 using Vaulty.Utils;
 
 namespace Vaulty.Modules
@@ -19,18 +20,9 @@ namespace Vaulty.Modules
 
             var info = JsonSensitiveLoader.BotInfoLoad();
 
-            var embed = new DiscordEmbedBuilder()
-            .WithTitle("Bot Information")
-            .WithColor(DiscordColor.Cyan)
-            .WithThumbnail(App.Vaulty._client.CurrentUser.AvatarUrl) // Bot's profile picture
-            .AddField("Name", info.name, true)
-            .AddField("Version", info.version, true)
-            .AddField("Description", info.description)
-            .WithFooter($"Requested by {ctx.User.GlobalName}", ctx.User.AvatarUrl)
-            .WithTimestamp(DateTime.UtcNow);
-
-            // Send the embed
-            await ctx.RespondAsync(embed);
+            InfoEmbed embed = new InfoEmbed(ctx, info.name, info.description, info.version);
+            
+            await ctx.RespondAsync(embed.builder.Build());
         }
     }
 }
